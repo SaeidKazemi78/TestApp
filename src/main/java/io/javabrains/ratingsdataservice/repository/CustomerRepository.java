ed47394ffb4249f4396cf4a6b1d25a769f788d72
sajad <mehrabisajad@outlook.com>
2020-08-04 10:27:53 +0430

    Last one

--- src/main/java/io/javabrains/ratingsdataservice/repository/CustomerRepository.java
+++ src/main/java/io/javabrains/ratingsdataservice/repository/CustomerRepository.java
@@ -1,9 +1,6 @@
 package io.javabrains.ratingsdataservice.repository;
 
-<<<<<<< HEAD
 import io.javabrains.ratingsdataservice.dto.CustomDto;
-=======
->>>>>>> 6c9f13455c7294105efe0ebb677cc0d710da047e
 import io.javabrains.ratingsdataservice.enums.CarType;
 import io.javabrains.ratingsdataservice.model.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -17,7 +14,7 @@ public interface CustomerRepository  extends JpaRepository<Customer,Long> {
 
     Customer findByName(String alley);
 
-<<<<<<< HEAD
+
     @Query("select new io.javabrains.ratingsdataservice.dto.CustomDto( customer.name,com.title )" +
             "from Customer customer " +
             " left join  Complex  com on  customer.complexId = com.id  ")
@@ -29,9 +26,11 @@ public interface CustomerRepository  extends JpaRepository<Customer,Long> {
     List<Customer> getByCarType(CarType carTypel);
 
 //select custom.name,custom.family from Custom costom inner join Compplex complex
-=======
+
 //    @Query("select customer from Customer")
 //    Object getByQuery(String name, CarType carType);
 
->>>>>>> 6c9f13455c7294105efe0ebb677cc0d710da047e
+
+
+
 }
