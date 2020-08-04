af96ebe3b4b1c9478b4925c6692d01b42f96d700
sajad <mehrabisajad@outlook.com>
2020-08-04 11:38:46 +0430

    develop new 11:38

--- src/main/java/io/javabrains/ratingsdataservice/repository/CustomerRepository.java
+++ src/main/java/io/javabrains/ratingsdataservice/repository/CustomerRepository.java
@@ -26,10 +26,10 @@ public interface CustomerRepository  extends JpaRepository<Customer,Long> {
     List<CustomDto> getByQuery();
 
 
-    //Here is develop change
+    //Here is develop changevasdsavas  sacvscass
 
 
-    //Here is develop
+    //Here is develop sdasdas as assdc
 
 //    @Query("select  cus from Customer  cus where cus.carType=:carType")
 //    List<Customer> getNullCustomers(CarType carType);
